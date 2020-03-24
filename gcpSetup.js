0499129bf39672b71e280967c429ec2d9a6b1fdd
Raphaël Côté <raphael.cote@ellipse-synergie.com>
2020-03-24 18:00:09 -0400

    Update gcpSetup.js

--- gcpSetup.js
+++ gcpSetup.js
@@ -1,2 +1,4 @@
-var fs = require('fs');
-fs.writeFileSync(process.env.GOOGLE_SERVICE_ACCOUNT_JSON, process.env.GCP_CRED);
+if (process.env.GOOGLE_SERVICE_ACCOUNT_JSON && process.env.GCP_CRED) {
+    var fs = require('fs');
+    fs.writeFileSync(process.env.GOOGLE_SERVICE_ACCOUNT_JSON, process.env.GCP_CRED);
+}
