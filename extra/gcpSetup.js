9d7182698ed3a8780cca008551d8ddeaff38d161
Raphaël Côté <raphael.cote@ellipse-synergie.com>
2020-03-24 22:11:46 -0400

    Update gcpSetup.js

--- extra/gcpSetup.js
+++ extra/gcpSetup.js
@@ -2,4 +2,6 @@ if (process.env.GOOGLE_SERVICE_ACCOUNT_FILENAME && process.env.GCP_CRED) {
     var fs = require('fs');
     var path = require('path');
     fs.writeFileSync(path.join(__dirname, '/../../' + process.env.GOOGLE_SERVICE_ACCOUNT_FILENAME), process.env.GCP_CRED);
+} else {
+    console.log("No files to copy, skipping setup");
 }
